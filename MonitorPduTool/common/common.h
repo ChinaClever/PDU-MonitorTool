e03a9a2e435ca5f676b7f58b10ab0905619c3dd7
luozhiyong <luozhiyong131@qq.com>
2020-10-29 15:27:31 +0800

    改一下Ping函数名

--- MonitorPduTool/common/common.h
+++ MonitorPduTool/common/common.h
@@ -3,7 +3,7 @@
 #include "msgbox.h"
 #include "config.h"
 
-bool cm_checkNet(const QString& ip);
+bool cm_pingNet(const QString& ip);
 bool cm_isDigitStr(const QString &src);
 bool cm_isIPaddress(const QString& ip);
 
