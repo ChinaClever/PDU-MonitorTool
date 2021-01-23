406fcf28a5c72d91c211c0c5a2aecad62b9899c6
luozhiyong <luozhiyong131@qq.com>
2021-01-23 15:18:19 +0800

    解决BUG

--- MonitorPduTool/cores/test_errrange.cpp
+++ MonitorPduTool/cores/test_errrange.cpp
@@ -166,8 +166,8 @@ bool Test_ErrRange::curAlarm(int id)
 
     bool ret = true;
     if((mDt->lines == 2) && id){
-        if(unit->min[id] != cth->cur_min/2) ret = false;
-        if(unit->max[id] != cth->cur_max/2) ret = false;
+        if(unit->min[id] != (cth->cur_min+1)/2) ret = false;
+        if(unit->max[id] != (cth->cur_max+1)/2) ret = false;
     }else{
         if(unit->min[id] != cth->cur_min) ret = false;
         if(unit->max[id] != cth->cur_max) ret = false;
