5721f7b68d782660fe9dcc3f604b3438f3506bc2
luozhiyong <luozhiyong131@qq.com>
2021-04-27 14:18:31 +0800

    质检IP读取版本号对比

--- PDU-MonitorTest/pyweb/ip_web.py
+++ PDU-MonitorTest/pyweb/ip_web.py
@@ -61,6 +61,15 @@ class IpWeb:
             self.setItById("psd", pwd)
             self.execJs("login()")
             self.sendtoMainapp("网页登陆成功", 1)
+            tt = self.driver.find_element_by_xpath('//div/div/div/div[last()]/span')
+            #print(tt.text)
+            name , ver = tt.text.split(':')
+            if( ver == self.cfgs['sw_ver'] ):
+                self.sendtoMainapp("版本号正确", 1)
+            elif( len(self.cfgs['sw_ver']) > 0 ):
+                self.sendtoMainapp("版本号空", 0)
+            else:
+                self.sendtoMainapp("版本号错误", 1)
         except:
             self.sendtoMainapp("网页登陆失败", 0)
         finally:
