a878802ecc67bbeaf4fa42bfd6cb8a15d653ea6e
luozhiyong <luozhiyong131@qq.com>
2021-11-19 11:14:30 +0800

    加多IPv6

--- PDU-MonitorTest/common/cfgcom/config.h
+++ PDU-MonitorTest/common/cfgcom/config.h
@@ -27,7 +27,8 @@ enum eIpTypes {
     IP_PDUV3_C3,        // IP-PDU 俄罗斯C3
     IP_PDUV3_EATON,     // IP-PDU伊顿
     IP_PDUV6,           // IPV6标准
-    IP_PDUV6_HUAWEI     // IPV6华为
+    IP_PDUV6_HUAWEI,     // IPV6华为
+    IP_PDUV6_ALI     // IPV6阿里
 };
 
 struct sIpCfg {
